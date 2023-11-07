b9f73b856711fd1e1225e3a0638efbcf9112e710
William Li <email@williamli.dev>
2023-11-07 18:49:45 +0800

    trying to confuse the system

--- app/catch-all-example/[[...slug]]/page.tsx
+++ app/catch-all-example/[[...slug]]/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-static'
-export const revalidate = 60;
+// export const dynamic = 'force-static'
+// export const revalidate = 60;
 
 export default function Page({ params }: { params: { slug: string[] } }) {
   const now = new Date().toISOString();
@@ -8,4 +8,13 @@ export default function Page({ params }: { params: { slug: string[] } }) {
     <div>Slug: /catch-all-example/{params.slug?.join('/')}</div>
     <div>Generated at: {now}</div>
   </>
+}
+
+export async function generateStaticParams() {
+
+  return [
+    { params: { slug: ['academy'] } },
+    { params: { slug: ['academy', 'glossar'] } },
+    { params: { slug: ['academy', 'glossar', 'index'] } }
+  ]
 }
