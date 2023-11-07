91d233de9e692b5becc7a7bc5cb1e2a685067d48
William Li <email@williamli.dev>
2023-11-07 19:01:33 +0800

    force static

--- app/catch-all-example/[[...slug]]/page.tsx
+++ app/catch-all-example/[[...slug]]/page.tsx
@@ -1,4 +1,4 @@
-// export const dynamic = 'force-static'
+export const dynamic = 'force-static'
 // export const revalidate = 60;
 
 export default function Page({ params }: { params: { slug: string[] } }) {
